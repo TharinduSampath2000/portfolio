53be96020c6a2a2eeeaf57486e4331bb626df3e9
TharinduSampath2000 <stharindu444@gmail.com>
2025-11-23 14:04:00 +0530

    Navbar and welcome screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,12 @@
-import { BrowserRouter } from "react-router-dom"
-import { About, Contact, Experience, Hero, Navbar, Tech, Works, StarsCanvas } from "./components"
+import { Navbar, Welcome } from "#components"
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <div className="relative z-0 bg-primary">
-        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-right">
-          <Navbar />
-          <Hero />
-        </div>
-        <About />
-        <Experience />
-        <Tech />
-        <Works />
-        {/* <Feedbacks /> */}
-        <div className="relative z-0">
-          <Contact />
-          <StarsCanvas />
-        </div>
-      </div>
-    </BrowserRouter>
-  )
+    <main>
+      <Navbar />
+      <Welcome />
+    </main>
+  );
 }
 
 export default App
